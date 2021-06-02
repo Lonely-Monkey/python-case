90a548e7b2bdd241326f274b0b0cf2fab5d7e373
hanhong.peng <hanhong.peng@xuncetech.com>
2021-06-02 13:30:45 +0800

    学生管理系统

--- studentsystem/studentsystem.py
+++ studentsystem/studentsystem.py
@@ -77,15 +77,15 @@ def insert():
         inputMark = input("是否继续添加？（y/n）:")
         if inputMark == "n":
             mark = False
-        save(studentList)
-        print("学生信息录入完毕！")
+            save(studentList)
+            print("学生信息录入完毕！")
 
 #删除学生信息
 def delete():
     mark = True
     while mark:
         studentId = input("请输入要删除的学生ID：")
-        if studentId is not "":
+        if studentId != "":
             #判断文件是否存在
             if os.path.exists("student"):
                 with open("student", 'r') as rfile:
@@ -127,9 +127,10 @@ def modify():
     else:
         return
     studentId = input("请输入要修改的学生ID：")
-    with open("student", 'r') as wfile:
+    with open("student", 'w') as wfile:
         for student in student_old:
             #字符串转字典
+            #eval()函数用于执行一个字符串表达式并且返回表达式的值
             d = dict(eval(student))
             if d["id"] == studentId:
                 print("找到了这名学生，可以修改他的信息!")
@@ -154,3 +155,120 @@ def modify():
     mark = input("是否继续修改其他学生信息？（y/n）：")
     if mark == "y":
         modify()
+
+#查找学生信息
+def search():
+    mark = True
+    student_query = []
+    while mark:
+        id = ""
+        name = ""
+        if os.path.exists("student"):
+            mode = input("按ID查输入1；按学生姓名查输入2：")
+            if mode == "1":
+                id = input("请输入学生ID：")
+            elif mode == "2":
+                name = input("请输入姓名：")
+            else:
+                print("您输入有误，请重新输入！")
+                search()
+            with open("student", 'r') as file:
+                #读取全部内容
+                student = file.readlines()
+                for list in student:
+                    #字符串转字典
+                    d = dict(eval(list))
+                    if d['id'] == id or d['name'] == name:
+                        student_query.append(d)
+                #显示查询结果
+                show_student(student_query)
+                student_query.clear()
+                inputMark = input("是否继续查询？（y/n）:")
+                if inputMark == "n":
+                    mark = False
+        else:
+            print("暂未缓存数据信息！")
+            return
+
+# 将保存在列表中的学生信息展示出来
+def show_student(studentList):
+    if not studentList:
+        print("(o@.@o) 无数据信息 （o@.@o） \n")
+        return
+    #定义标题显示格式
+    format_title = "{:^6}{:^12}\t{:^8}\t{:^10}\t{:^10}\t{:^10}"
+    #使用format方法对其进行格式化，数字：所占列宽，^：居中显示，\t：添加一个制表符
+    print(format_title.format("ID", "名字", '英语成绩', 'python成绩', 'c语言成绩', '总成绩'))
+    #定义具体内容显示格式
+    format_data = "{:^8}{:^10}\t{:^10}\t{:^10}\t{:^12}\t{:^12}"
+    for info in studentList:
+        print(format_data.format(info.get('id'),
+                                 info.get('name'),
+                                 str(info.get('english')),
+                                 str(info.get('python')),
+                                 str(info.get('c')),
+                                 str(info.get('english') + info.get('python') + info.get('c')).center(12)))
+
+#统计学生总数
+def total():
+    if os.path.exists('student'):
+        with open('student', 'r') as rfile:
+            student_old = rfile.readlines()
+            if student_old:
+                #统计学生人数
+                print("一共有 %d 名学生！" % len(student_old))
+            else:
+                print("还没有录入学生信息！")
+    else:
+        print('暂未保存数据信息.....')
+
+#显示所有学生信息
+def show():
+    student_new = []
+    if os.path.exists('student'):
+        with open('student', 'r') as rfile:
+            student_old = rfile.readlines()
+        for list in student_old:
+            student_new.append(eval(list))
+        if student_new:
+            show_student(student_new)
+    else:
+        print('暂未保存数据信息....')
+
+#排序
+def sort():
+    show()
+    if os.path.exists('student'):
+        with open('student', 'r') as file:
+            student_old = file.readlines()
+            student_new = []
+        for list in student_old:
+            d = dict(eval(list))
+            student_new.append(d)
+    else:
+        return
+    ascORdesc = input("请选择（0升序；1降序）：")
+    if ascORdesc == '0':
+        ascORdescBool = False
+    elif ascORdesc == '1':
+        ascORdescBool = True
+    else:
+        print("您输入有误，请重新输入！")
+        sort()
+    mode = input('请选择排序方式（1按英语成绩排序；2按python成绩排序；3按c语言成绩排序；0按总成绩排序）：')
+    if mode == '1':
+        #key：排序的键；reverse：是否为降序排序
+        student_new.sort(key=lambda x: x['english'], reverse=ascORdescBool)
+    elif mode == '2':
+        student_new.sort(key=lambda x: x['python'], reverse=ascORdescBool)
+    elif mode == '3':
+        student_new.sort(key=lambda x: x['c'], reverse=ascORdescBool)
+    elif mode == '0':
+        student_new.sort(key=lambda x: x['english'] + x['python'] + x['c'], reverse=ascORdescBool)
+    else:
+        print('您的输入有误，请重新输入！')
+        sort()
+    show_student(student_new)
+
+if __name__ == '__main__':
+    main()
